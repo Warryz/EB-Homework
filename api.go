1810bc737881953606a63d30f3a45fd4ef81fa64
Warryz <8317771+Warryz@users.noreply.github.com>
2020-02-05 20:44:53 +0100

    Added database connection.

--- api.go
+++ api.go
@@ -4,20 +4,26 @@ package main
 // See: https://github.com/TutorialEdge/create-rest-api-in-go-tutorial
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
+)
+
+var (
+	db *sql.DB
 )
 
 // Customer - struct for customer data
 type Customer struct {
 	ID        int    `json:"Id"`
-	surname   string `json:"Title"`
-	givenname string `json:"desc"`
+	Surname   string `json:"Title"`
+	Givenname string `json:"desc"`
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -25,57 +31,79 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: Main API page")
 }
 
-func returnCustomerArticle(w http.ResponseWriter, r *http.Request) {
+func returnCustomer(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	key := vars["id"]
+	customerID, err := strconv.ParseInt(vars["id"], 10, 32)
 
-	for _, article := range Articles {
-		if article.ID == key {
-			json.NewEncoder(w).Encode(article)
+	if err != nil {
+		println("Error while parsing a customer id!")
+	} else {
+		// Prepare statement for reading data
+		stmtOut, err := db.Prepare("SELECT Customers_ID, Surname, Givenname FROM Customers WHERE Customer_ID = ?")
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
 		}
+		defer stmtOut.Close()
+
+		var customerData Customer // we "scan" the result in here
+
+		// Query the square-number of the customer id store it in customerdata
+		err = stmtOut.QueryRow(customerID).Scan(&customerData.ID, &customerData.Surname, customerData.Givenname)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		fmt.Printf("The name of customer %d is: %s %s", customerData.ID, customerData.Givenname, customerData.Surname)
+
 	}
+
 }
 
-func createNewArticle(w http.ResponseWriter, r *http.Request) {
+func createNewCustomer(w http.ResponseWriter, r *http.Request) {
 	// get the body of our POST request
 	// unmarshal this into a new Article struct
 	// append this to our Articles array.
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	var article Article
-	json.Unmarshal(reqBody, &article)
+	var cus Customer
+	json.Unmarshal(reqBody, &cus)
 	// update our global Articles array to include
 	// our new Article
-	Articles = append(Articles, article)
 
-	json.NewEncoder(w).Encode(article)
+	json.NewEncoder(w).Encode(cus)
 }
 
-func deleteArticle(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
+// func deleteArticle(w http.ResponseWriter, r *http.Request) {
+// 	vars := mux.Vars(r)
+// 	id := vars["id"]
 
-	for index, article := range Articles {
-		if article.ID == id {
-			Articles = append(Articles[:index], Articles[index+1:]...)
-		}
-	}
+// 	for index, article := range Articles {
+// 		if article.ID == id {
+// 			Articles = append(Articles[:index], Articles[index+1:]...)
+// 		}
+// 	}
 
-}
+// }
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/articles", returnAllArticles)
-	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
-	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	// myRouter.HandleFunc("/articles", returnAllArticles)
+	// myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	// myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/customer/{id}", returnCustomer)
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
-	Articles = []Article{
-		Article{ID: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{ID: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+
+	db, err := sql.Open("mysql", "admin:admin@tcp(123.4.5.6:3306)/database-1")
+	if err != nil {
+		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
 	}
 	handleRequests()
 }
