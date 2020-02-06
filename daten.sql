5bdcdef06a42276f04282ac2710bc3fad3c3373f
Warryz <8317771+Warryz@users.noreply.github.com>
2020-02-06 21:45:53 +0100

    Updated the sql file for data.

--- daten.sql
+++ daten.sql
@@ -1,12 +1,18 @@
-CREATE TABLE IF NOT EXISTS "Readings" (
-	"Customers_ID_FK"	INTEGER,
-	"Measure_Date"	TEXT NOT NULL,
-	"Value"	INTEGER NOT NULL,
-	PRIMARY KEY("Customers_ID_FK"),
-	FOREIGN KEY("Customers_ID_FK") REFERENCES "Customers"("Customers_ID")
+DROP DATABASE IF EXISTS hausarbeit;
+CREATE DATABASE hausarbeit; USE hausarbeit;
+
+CREATE TABLE IF NOT EXISTS Customers (
+	Customers_ID INT UNSIGNED AUTO_INCREMENT,
+	Surname VARCHAR(60) NOT NULL,
+	Givenname VARCHAR(60) NOT NULL,
+	PRIMARY KEY(Customers_ID)
 );
-CREATE TABLE IF NOT EXISTS "Customers" (
-	"Customers_ID"	INTEGER PRIMARY KEY AUTOINCREMENT,
-	"Surname"	TEXT NOT NULL,
-	"Givenname"	TEXT NOT NULL
+
+CREATE TABLE IF NOT EXISTS Readings (
+	Customers_ID_FK INT UNSIGNED,
+	Measure_ID INT UNSIGNED,
+	Measure_Date DATETIME NOT NULL,
+	Value INT NOT NULL, 
+	PRIMARY KEY(Customers_ID_FK, Measure_ID), 
+	FOREIGN KEY(Customers_ID_FK) REFERENCES Customers(Customers_ID)
 );
