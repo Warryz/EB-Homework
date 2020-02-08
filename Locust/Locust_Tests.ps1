9538a6058bfa7ef7408d89f17394c7b6d2b521c0
Warryz <8317771+Warryz@users.noreply.github.com>
2020-02-08 11:18:10 +0100

    Added a Hostname Parameter

--- Locust/Locust_Tests.ps1
+++ Locust/Locust_Tests.ps1
@@ -1,3 +1,7 @@
+param(
+    $Hostname
+)
+
 # This will be written to the file name.
 $testCase = "three_tier"
 
@@ -5,9 +9,11 @@ $numberOfUsersToSimulate = 10, 50, 100, 200, 400, 800, 1500
 $maxNumerOfRequests = 50000
 
 # AWS Hostname
-$ipAddress = Read-Host -Prompt "Please enter the AWS Hostname"
+if ($null -eq $Host) {
+    $ipAddress = Read-Host -Prompt "Please enter the AWS Hostname"
+}
 
-foreach($users in $numberOfUsersToSimulate){
+foreach ($users in $numberOfUsersToSimulate) {
     $testWithUsers = $testCase + "_" + $users
     <#
     Parameters: 
