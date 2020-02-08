4adc3a9c910f3bd2bdba54b192d9a12035444d43
Warryz <8317771+Warryz@users.noreply.github.com>
2020-02-08 16:15:22 +0100

    Updated the locust test files script.

--- Locust/Locust_Tests.ps1
+++ Locust/Locust_Tests.ps1
@@ -5,12 +5,11 @@ param(
 # This will be written to the file name.
 $testCase = "three_tier"
 
-$numberOfUsersToSimulate = 10, 50, 100, 200, 400, 800, 1500
-$maxNumerOfRequests = 50000
+$numberOfUsersToSimulate = 50, 100, 200, 400, 800, 1500
 
 # AWS Hostname
 if ($null -eq $Host) {
-    $ipAddress = Read-Host -Prompt "Please enter the AWS Hostname"
+    $Hostname = Read-Host -Prompt "Please enter the AWS Hostname"
 }
 
 foreach ($users in $numberOfUsersToSimulate) {
@@ -23,5 +22,5 @@ foreach ($users in $numberOfUsersToSimulate) {
     #>
     
     # Executing the Python file.
-    locust -f .\Load_Test.py --no-web -c $users -r 10 -n $maxNumerOfRequests --csv=..\Locust-Ergebnisse\$testWithUsers --host="http://$($ipAddress):10000"
+    locust -f .\Locust\Load_Test.py --no-web -c $users -r 10 --step-load --step-clients ($users/10) --step-time 15s -t 3m --csv=Ergebnisse/$testWithUsers --host="http://$($Hostname):10000" --only-summary
 }
