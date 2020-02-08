eb59547af2b0f767e5acb490f9c706000690b07a
Warryz <8317771+Warryz@users.noreply.github.com>
2020-02-08 11:13:25 +0100

    Added the load test script.

---
+++ Locust/Load_Test.py
@@ -0,0 +1,22 @@
+'''
+Accessing the website by locust -f .\Load_Test.py --host=http://192.168.2.15
+'''
+from locust import HttpLocust, TaskSet
+from random import randrange
+print(randrange(10))
+
+# Opening the website http://example.com/Hausarbeit
+def index(l):
+    l.client.get(f'/customerdata/{randrange(5_000)}')
+    # print("Accessing the website.")
+
+class UserBehavior(TaskSet):
+    tasks = {index: 1}
+
+    # Things to do before doing anything else.
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    # Definition of the user behavior: Wait at least 5 seconds and maximum 9 seconds. Default is 1 second.
+    wait_time = between(5.0, 9.0)
