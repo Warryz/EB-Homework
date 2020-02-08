9a5adc623a3ae898cd5d78c92d0cd625a1fbb691
Warryz <8317771+Warryz@users.noreply.github.com>
2020-02-08 16:14:54 +0100

    Updated the load test python script.

--- Locust/Load_Test.py
+++ Locust/Load_Test.py
@@ -1,13 +1,13 @@
 '''
 Accessing the website by locust -f .\Load_Test.py --host=http://192.168.2.15
 '''
-from locust import HttpLocust, TaskSet
+from locust import HttpLocust, TaskSet, between
 from random import randrange
 print(randrange(10))
 
 # Opening the website http://example.com/Hausarbeit
 def index(l):
-    l.client.get(f'/customerdata/{randrange(5_000)}')
+    l.client.get(f'/customerdata/{randrange(4500)}')
     # print("Accessing the website.")
 
 class UserBehavior(TaskSet):
@@ -18,5 +18,5 @@ class UserBehavior(TaskSet):
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
-    # Definition of the user behavior: Wait at least 5 seconds and maximum 9 seconds. Default is 1 second.
+    # Definition of the user behavior: Wait at least 5 seconds and maximum 9 seconds. 
     wait_time = between(5.0, 9.0)
