982227dbbee05183538c21c3dbbbee01035dabc8
Warryz <8317771+Warryz@users.noreply.github.com>
2020-02-10 19:31:45 +0100

    Moved to aws lambda.

--- api.go
+++ api.go
@@ -3,17 +3,15 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
-	"time"
+	"regexp"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/bradfitz/gomemcache/memcache"
-
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
 )
 
 var (
@@ -46,221 +44,76 @@ func (reading *myReadings) AddItem(item Readings) {
 	reading.Measures = append(reading.Measures, item)
 }
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the API!")
-	fmt.Println("Endpoint Hit: Main API page")
+// Similarly add a helper for send responses relating to client errors.
+func clientError(status int) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body:       http.StatusText(status),
+	}, nil
 }
 
-func returnCustomerData(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	customerID, parseErr := strconv.ParseInt(vars["id"], 10, 32)
+func returnCustomerData(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// ID may only contain numbers
+	var idRegExp = regexp.MustCompile(`[0-9]`)
 
-	if parseErr != nil {
-		println("dbError while parsing a customer id!")
-	} else {
-		// Try reading the data from the memcached server
-		key := fmt.Sprintf("customerReadings_id_%d", customerID)
+	// Parse the id from the query string
+	ID := req.QueryStringParameters["id"]
 
-		// mc.Get(&memcache.Item{Key: key, Value: []byte(b)})
-		it, memErr := mc.Get(key)
-		if memErr != nil {
-			fmt.Printf("No data for customer id %d in memcached: %s", customerID, memErr)
-
-			// Prepare statement for reading data
-			stmtOut, dbErr := db.Prepare("SELECT Measure_ID, Measure_Date, Value FROM Readings WHERE Customers_ID_FK = ?;")
-			if dbErr != nil {
-				fmt.Println("Error while creating the sql statement")
-			}
-			defer stmtOut.Close()
-
-			// Query the customer id store it in customerdata
-
-			rows, dbErr := stmtOut.Query(customerID)
-			defer rows.Close()
-
-			customReadingsList := myReadings{}
-			var customerReadings Readings
-
-			if dbErr != nil {
-				fmt.Println("unable to query user data", customerID, dbErr)
-			} else {
-				for rows.Next() {
-
-					err := rows.Scan(&customerReadings.MeasureID, &customerReadings.MeasureDate, &customerReadings.MeasureValue)
-					if err != nil {
-						log.Fatal(err)
-					}
-					// https://stackoverflow.com/questions/18042439/go-append-to-slice-in-struct
-					customReadingsList.AddItem(customerReadings)
-				}
-				json.NewEncoder(w).Encode(customReadingsList)
-			}
-		} else {
-			// Output the memcached data.
-			w.Write(it.Value)
-		}
-
+	// Check if the provided ID is valid
+	if !idRegExp.MatchString(ID) {
+		return clientError(http.StatusBadRequest)
 	}
+	key := fmt.Sprintf("customerReadings_id_%s", ID)
 
-}
+	it, memErr := mc.Get(key)
+	if memErr != nil {
+		fmt.Printf("No data for customer id %s in memcached: %s", ID, memErr)
 
-func returnCustomer(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	customerID, parseErr := strconv.ParseInt(vars["id"], 10, 32)
+		// Create a db connection
+		initSetup()
 
-	if parseErr != nil {
-		println("dbError while parsing a customer id!")
-	} else {
 		// Prepare statement for reading data
-		stmtOut, dbErr := db.Prepare("SELECT Customers_ID, Surname, Givenname FROM Customers WHERE Customers_ID = ?")
+		stmtOut, dbErr := db.Prepare("SELECT Measure_ID, Measure_Date, Value FROM Readings WHERE Customers_ID_FK = ?;")
 		if dbErr != nil {
 			fmt.Println("Error while creating the sql statement")
 		}
 		defer stmtOut.Close()
 
-		var customerData Customer // we "scan" the result in here
-
 		// Query the customer id store it in customerdata
-		dbErr = stmtOut.QueryRow(customerID).Scan(&customerData.ID, &customerData.Surname, &customerData.Givenname)
+		rows, dbErr := stmtOut.Query(ID)
+		defer rows.Close()
+
+		customReadingsList := myReadings{}
+		var customerReadings Readings
+
 		if dbErr != nil {
-			fmt.Println("unable to query user", customerID, dbErr)
+			fmt.Println("unable to query user data", ID, dbErr)
 		} else {
-			fmt.Printf("The name of customer %d is: %s %s", customerData.ID, customerData.Givenname, customerData.Surname)
+			for rows.Next() {
 
-			json.NewEncoder(w).Encode(customerData)
-		}
-	}
-
-}
-
-func handleRequests(webserverPort int) {
-	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/customer/{id}", returnCustomer)
-	myRouter.HandleFunc("/customerdata/{id}", returnCustomerData)
-
-	// Needed to disable connection timeouts
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", webserverPort),
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		Handler:      myRouter,
-	}
-
-	srv.SetKeepAlivesEnabled(false)
-	fmt.Printf("Starting webserver on port: %d", webserverPort)
-	log.Fatal(srv.ListenAndServe())
-}
-
-func loadCustomerDataIntoMemory() {
-	// Load the customer data into cached
-	stmtOut, dbErr := db.Prepare("SELECT * FROM Customers;")
-	if dbErr != nil {
-		fmt.Println("Error while creating the sql statement")
-	}
-	defer stmtOut.Close()
-
-	// Query the customer id store it in customerdata
-	rows, dbErr := stmtOut.Query()
-	defer rows.Close()
-
-	var customerData Customer // we "scan" the result in here
-
-	if dbErr != nil {
-		fmt.Println("unable to load user into memcached", dbErr)
-	} else {
-
-		for rows.Next() {
-			// ID, Surname, givenname
-			err := rows.Scan(&customerData.ID, &customerData.Surname, &customerData.Givenname)
-			if err != nil {
-				fmt.Println("unable to parse user row into memcached", err)
-			}
-
-			// Save the loaded data to memcached by converting it to json: https://stackoverflow.com/questions/8270816/converting-go-struct-to-json
-			b, err := json.Marshal(customerData)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-			// Format the key and
-			key := fmt.Sprintf("customerData_id_%d", customerData.ID)
-			//  Save the data to memcached servers
-			mc.Set(&memcache.Item{Key: key, Value: []byte(b)})
-		}
-		fmt.Println("Finished cache creation for customer data.")
-	}
-
-}
-
-func loadReadingsDataIntoMemory() {
-	// Load the readings data into cache
-
-	// Does not work, we need this user by user
-
-	stmtOut, dbErr := db.Prepare("SELECT DISTINCT Customers_ID_FK FROM Readings;")
-	if dbErr != nil {
-		fmt.Println("Error while creating the sql statement")
-	}
-	defer stmtOut.Close()
-
-	// Query the customer id store it in customerdata
-	rows, dbErr := stmtOut.Query()
-	defer rows.Close()
-
-	if dbErr != nil {
-		fmt.Println("unable to query user ids for caching memcached", dbErr)
-	} else {
-		// Load the user ids
-		for rows.Next() {
-			customReadingsList := myReadings{}
-			var customerReadings Readings
-			var customerID int
-
-			err := rows.Scan(&customerID)
-			if err != nil {
-				// log.Fatal(err)
-				fmt.Println("Could not parse user id")
-				continue
-			}
-
-			// stmtOut, dbErr := db.Prepare("SELECT DISTINCT Customers_ID_FK FROM Readings;")
-			stmtOut, dbErr := db.Prepare("SELECT Measure_ID, Measure_Date, Value FROM Readings where Customers_ID_FK = ?;")
-			if dbErr != nil {
-				fmt.Println("Error while creating the sql statement")
-			}
-			defer stmtOut.Close()
-
-			// Query the customer id store it in customerdata
-			readingRows, dbErr := stmtOut.Query(customerID)
-			defer rows.Close()
-
-			if dbErr != nil {
-				fmt.Println("unable to query user ids for caching memcached", dbErr)
-			} else {
-				for readingRows.Next() {
-					// IDFK, MeasureID, Date, Value
-					err := readingRows.Scan(&customerReadings.MeasureID, &customerReadings.MeasureDate, &customerReadings.MeasureValue)
-					if err != nil {
-						log.Fatal(err)
-					}
-					// https://stackoverflow.com/questions/18042439/go-append-to-slice-in-struct
-					customReadingsList.AddItem(customerReadings)
-				}
-				// Save the loaded data to memcached by converting it to json: https://stackoverflow.com/questions/8270816/converting-go-struct-to-json
-				b, err := json.Marshal(customReadingsList)
+				err := rows.Scan(&customerReadings.MeasureID, &customerReadings.MeasureDate, &customerReadings.MeasureValue)
 				if err != nil {
-					fmt.Println(err)
-					continue
+					log.Fatal(err)
 				}
-				// Format the key and
-				key := fmt.Sprintf("customerReadings_id_%d", customerID)
-				//  Save the data to memcached servers
-				mc.Set(&memcache.Item{Key: key, Value: []byte(b)})
+				// https://stackoverflow.com/questions/18042439/go-append-to-slice-in-struct
+				customReadingsList.AddItem(customerReadings)
+			}
+			json, err := json.Marshal(customReadingsList)
+			if err != nil {
+				return events.APIGatewayProxyResponse{
+					StatusCode: http.StatusOK,
+					Body:       string(json),
+				}, nil
 			}
 		}
 	}
-	fmt.Println("Finished cache creation for customer readings.")
+
+	// Return the events and a http 200 code.
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(it.Value),
+	}, nil
+
 }
 
 func initSetup() {
@@ -278,19 +131,9 @@ func initSetup() {
 		fmt.Println("DB connection established!")
 	}
 
-	// Load all from the db into memcached
-	loadCustomerDataIntoMemory()
-	loadReadingsDataIntoMemory()
-
-	// Print when ready to serve
-	fmt.Println("Ready to serve traffic...")
 }
 
 func main() {
-	// Port flag
-	portPtr := flag.Int("port", 10000, "Port for the webserver to start")
-	flag.Parse()
-
-	initSetup()
-	handleRequests(*portPtr)
+	// Start the Lambda Handler
+	lambda.Start(returnCustomerData)
 }
